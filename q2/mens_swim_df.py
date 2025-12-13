6638614ce1c86faa6203ec5039a93db13ee84f4e
Rhea Augustine <rhea.augustine@student.mec.cuny.edu>
2025-12-13 14:39:26 -0500

    Add men's swimming dataframe and CSV (Member 1)

---
+++ q2/mens_swim_df.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Men's Swimming DataFrame (Member 1 responsibility)
+data = [
+    {"school": "College of Staten Island", "name": "Sample Swimmer A", "height": "6-1"},
+    {"school": "York College", "name": "Sample Swimmer B", "height": "5-11"},
+    {"school": "Baruch College", "name": "Sample Swimmer C", "height": "6-3"},
+]
+
+df_mens_swim = pd.DataFrame(data)
+
+print("Men's Swimming DataFrame:")
+print(df_mens_swim)
+
+# Save dataframe to CSV
+df_mens_swim.to_csv("../data/mens_swim_sample.csv", index=False)
+print("Saved CSV to data/mens_swim_sample.csv")
